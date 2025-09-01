8b1ae28d474aec4e687e780603fb3e69bf89b027
Bruno Righi <brunorighi267@gmail.com>
2025-09-01 10:54:05 -0300

    update

---
+++ Sync_e_Async/Async/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Café da manhã");
+CafeDaManha();
+Console.ReadKey();
+
+static async void CafeDaManha()
+{
+    Console.WriteLine("\nPreparar o café:");
+    var Taskcafe = PrepararCafe();
+    Console.WriteLine("\nPreparar o pão:");
+    var Taskpao = PrepararPao();
+    var cafe = await (Taskcafe);
+    var pao = await (Taskpao);
+    ServirCafe(cafe, pao);
+}
+
+static async Task<Cafe> PrepararCafe()
+{
+    Console.WriteLine("Ferver a água");
+    await Task.Delay(2000);
+    Console.WriteLine("Coando o café");
+    await Task.Delay(2000);
+    return new Cafe();
+}
+
+static async Task<Pao> PrepararPao()
+{
+    Console.WriteLine("Partir o pão");
+    await Task.Delay(2000);
+    Console.WriteLine("Passar manteiga no pão");
+    await Task.Delay(2000);
+    Console.WriteLine("Tostar o pão");
+    return new Pao();
+}
+
+static void ServirCafe(Cafe cafe, Pao pao)
+{
+    Console.WriteLine("\nServindo o café da manhã");
+    Thread.Sleep(2000);
+    Console.WriteLine("Café servido");
+}
+
+internal class Cafe()
+{
+}
+internal class Pao()
+{
+}

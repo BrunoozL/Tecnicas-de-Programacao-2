3a79604d9bd33e00584bbc33a42e2e723911d48c
Bruno Righi <brunorighi267@gmail.com>
2025-09-29 10:32:09 -0300

    apendendo sobre Delegate e Evento

---
+++ Delegate_Evento/Evento/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Iniciando o Pedido");
+Pedido pedido = new Pedido();
+
+// Registra os assinantes
+pedido.OnCriarPedido += EnviarEmail.Email;
+pedido.OnCriarPedido += EnviarSMS.SMS;
+
+// Criar o pedido
+pedido.CriarPedido();
+Console.WriteLine("Finalizando o pedido");
+
+Console.ReadKey();
+
+public delegate void PedidoEvent();
+
+class Pedido
+{
+    public event PedidoEvent? OnCriarPedido;
+
+    public void CriarPedido()
+    {
+        Console.WriteLine("Pedido sendo criado");
+        if (OnCriarPedido != null)
+        {
+            OnCriarPedido();
+        }
+    }
+}
+
+class EnviarEmail
+{
+    public static void Email()
+    {
+        Console.WriteLine("Email enviado");
+    }
+}
+
+class EnviarSMS
+{
+    public static void SMS()
+    {
+        Console.WriteLine("SMS enviado");
+    }
+}
